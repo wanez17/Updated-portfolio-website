8ddaeb557f2f351e839fb63ebce9ff1ec7908ecb
Mohammed Awal <77703552+wanez17@users.noreply.github.com>
2023-01-30 10:14:52 +0800

    minor updates
    update of the hamburgar icon in both html css and js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var x =document.getElementById("navbar");
+var x = document.getElementById("navbar");
 
 function toggleMenu() {
-  if (x.style.display = "block") {
+  if (x.style.display == "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
